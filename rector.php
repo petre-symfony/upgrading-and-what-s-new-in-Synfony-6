7710ce2a8314cc3364b376f4f9cc451b91cba6b8
petrero <petrero@gmail.com>
2022-05-30 09:25:25 +0300

    3.3. Automatic Upgrades with Rector
      Configuring Rector for the Symfony Upgrade
      - If you look back at the documentation, you'll see a link to a Symfony repository where it tells you about a bunch of Symfony "rules" - fancy word for "upgrades" - that they've already prepared! That was nice of them!
    
      Below, copy the inside of their callback function... and paste it over what we have.

--- rector.php
+++ rector.php
@@ -7,15 +7,11 @@ use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-	$rectorConfig->paths([
-		__DIR__ . '/src'
+	$rectorConfig->symfonyContainerXml(__DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml');
+
+	$rectorConfig->sets([
+		SymfonySetList::SYMFONY_52,
+		SymfonySetList::SYMFONY_CODE_QUALITY,
+		SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
 	]);
-
-	// register a single rule
-	$rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-
-	// define sets of rules
-	//    $rectorConfig->sets([
-	//        LevelSetList::UP_TO_PHP_74
-	//    ]);
 };
