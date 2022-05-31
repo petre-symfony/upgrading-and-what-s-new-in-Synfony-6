42cffd6371676ff68431d234636e8b265ce00382
petrero <petrero@gmail.com>
2022-05-31 09:41:59 +0300

    9.1. Upgrading to PHP 8
      Let's keep track of our goal. Now that we've upgraded to Symfony 5.4, as soon as we remove all of these deprecations, we can safely upgrade to Symfony 6. But Symfony 6 requires PHP 8, and I've been building this project in PHP 7. So the next step is to update our code to be PHP 8 compatible. In practice, that means updating parts of our code to use some cool new PHP 8 features. Woo! And this is another spot where Rector can help us.
    
      Rector Upgrading To PHP 8!
      - Start by opening up rector.php and removing the three Symfony upgrade lines. Replace these with LevelSetList::UP_TO_PHP_80. Just like with Symfony, you can upgrade specifically to PHP 7.3 or 7.4, but they have these nice UP_TO_[...] statements that will upgrade our code across all versions of PHP up to PHP 8.0.
    
      And... that's all we need!

--- rector.php
+++ rector.php
@@ -3,15 +3,10 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Symfony\Set\SymfonyLevelSetList;
-use Rector\Symfony\Set\SymfonySetList;
+use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
 	$rectorConfig->symfonyContainerXml(__DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml');
 
-	$rectorConfig->sets([
-		SymfonyLevelSetList::UP_TO_SYMFONY_54,
-		SymfonySetList::SYMFONY_CODE_QUALITY,
-		SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-	]);
+	$rectorConfig->import(LevelSetList::UP_TO_PHP_80);
 };
