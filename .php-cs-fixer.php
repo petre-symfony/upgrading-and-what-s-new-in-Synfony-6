3a71c524ce0a327e1604958f05959335e9c3efde
petrero <petrero@gmail.com>
2022-05-31 12:31:30 +0300

    10.5. Annotations to Attributes   I like the line breaks between my entity properties.
      We could fix this by hand... but I'm wondering if we can teach PHP CS Fixer to do this for us.
    
      Open php-cs-fixer.php. The rule that controls these line breaks is called class_attributes_separation with an "s" - I'll fix that in a minute. Set this to an array that describes all of the different parts of our class and how each should behave. For example, we can say ['method' => 'one'] to say that we want one empty line between each method. We can also say ['property' => 'one'] to have one line break between our properties. There's also another called trait_import. Set that to one too. That gives us an empty line between our trait imports, which is something that we have on top of Answer.
    
      Now try php-cs-fixer again:
    
        tools/php-cs-fixer/vendor/bin/php-cs-fixer fix
      Whoops!
    
        The rules contain unknown fixers: "class_attribute_separation"
    
      I meant to say class_attributes_separation with an "s". What a great error though.

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -7,6 +7,9 @@ $config = new PhpCsFixer\Config();
 return $config->setRules([
 	'@Symfony' => true,
 	'yoda_style' => false,
+	'class_attribute_separation' => [
+		'elements' => ['method' => 'one', 'property' => 'one', 'trait_import' => 'one']
+	]
 ])
 	->setFinder($finder)
 ;
