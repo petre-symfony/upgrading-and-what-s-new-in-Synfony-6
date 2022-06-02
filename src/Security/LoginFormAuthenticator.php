8a5c917c0ac6d2a3204e37f9d89c989afe3840c1
petrero <petrero@gmail.com>
2022-06-02 08:09:32 +0300

    13.4. Custom Authenticator authenticate() Method
      Or, to always enable it, add ->enable() on the badge
    
      And, of course, none of this will work unless you activate the remember_me system under your firewall, which I don't actually have yet. It's still safe to add that badge... but there won't be any system to process it and add the cookie. So, the badge will be ignored.
    
      Deleting Old Methods!
      - Anyways, we're done! If that felt overwhelming, back up and watch our Symfony Security tutorial to get more context.
    
      The cool thing is that we don't need getCredentials(), getUser(), checkCredentials(), or getPassword() anymore. All we need is authenticate(), onAuthenticationSuccess(), onAuthenticationFailure(), and getLoginUrl(). We can even celebrate up here by removing a bunch of old use statements. Yay!
    
      Oh, and look at the constructor. We no longer need CsrfTokenManagerInterface or UserPasswordHasherInterface: both of those checks are now done somewhere else. And... that gives us two more use statements to delete.
    
      Activating the New Security System
      - At this point, our one custom authenticator has been moved to the new authenticator system. This mean that, in security.yaml, we are ready to switch to the new system! Say enable_authenticator_manager: true.
    
      And these custom authenticators aren't under a guard key anymore. Instead, add custom_authenticator and add this directly below that.
    
      Okay, moment of truth! We just completely switched to the new system. Will it work? Head back to the homepage, reload and... it does! And check out those deprecations! It went from around 45 to 4. Woh!
    
      Some of those relate to one more security change. Next: let's update to the new password_hasher & check out a new command for debugging security firewalls.

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -8,19 +8,10 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
-use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Csrf\CsrfToken;
-use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
-use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
-use Symfony\Component\Security\Guard\PasswordAuthenticatedInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
@@ -34,7 +25,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator {
 
 	public const LOGIN_ROUTE = 'app_login';
 
-	public function __construct(private SessionInterface $session, private EntityManagerInterface $entityManager, private UrlGeneratorInterface $urlGenerator, private CsrfTokenManagerInterface $csrfTokenManager, private UserPasswordHasherInterface $passwordHasher) {
+	public function __construct(private SessionInterface $session, private EntityManagerInterface $entityManager, private UrlGeneratorInterface $urlGenerator) {
 	}
 
 	public function authenticate(Request $request): Passport {
@@ -56,49 +47,9 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator {
 			new PasswordCredentials($password),
 			[
 				new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
-				(new RememberMeBadge())
+				(new RememberMeBadge())->enable()
 			]
 		);
-	}
-
-	public function getCredentials(Request $request) {
-		$credentials = [
-			'email' => $request->request->get('email'),
-			'password' => $request->request->get('password'),
-			'csrf_token' => $request->request->get('_csrf_token'),
-		];
-		$this->session->set(
-			Security::LAST_USERNAME,
-			$credentials['email']
-		);
-
-		return $credentials;
-	}
-
-	public function getUser($credentials, UserProviderInterface $userProvider) {
-		$token = new CsrfToken('authenticate', $credentials['csrf_token']);
-		if (!$this->csrfTokenManager->isTokenValid($token)) {
-			throw new InvalidCsrfTokenException();
-		}
-
-		$user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $credentials['email']]);
-
-		if (!$user) {
-			throw new UserNotFoundException('Email could not be found.');
-		}
-
-		return $user;
-	}
-
-	public function checkCredentials($credentials, UserInterface $user) {
-		return $this->passwordHasher->isPasswordValid($user, $credentials['password']);
-	}
-
-	/**
-	 * Used to upgrade (rehash) the user's password automatically over time.
-	 */
-	public function getPassword($credentials): ?string {
-		return $credentials['password'];
 	}
 
 	public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewalName): ?Response {
