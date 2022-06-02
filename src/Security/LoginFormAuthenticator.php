d51ae09fd9566060cda679c48d0cf2124cab046a
petrero <petrero@gmail.com>
2022-06-02 07:59:34 +0300

    13.2. Custom Authenticator authenticate() Method
      Optional Custom User Query
      - In the old system, we did this all manually by querying the UserRepository. That is not needed anymore. But sometimes... if you have custom logic, you might still need to find the user manually.
    
      If you have this use-case, pass a function() to the second argument that accepts a $userIdentifier argument. Now, when the authentication system needs the User object, it will call our function and pass us the "user identifier"... which will be whatever we passed to the first argument. So, the email.
    
      Our job is to use that to return the user. Start with $user = $this->entityManager->getRepository(User::class)
    
      And yea, I could have injected the UserRepository instead of the entity manager... that would be better... but this is fine. Then ->findOneBy(['email' => $userIdentifier]).
    
      If we did not find a user, we need to throw a new UserNotFoundException(). Then, return $user.
    
      First Passport argument is done!
    
      PasswordCredentials
      - For the second argument, down here, change my bad semicolon to a comma - then say new PasswordCredentials() and pass this the submitted $password.
    
      That's all we need! That's right: we do not need to actually check the password! We pass a PasswordCredentials()... and then another system is responsible for checking the submitted password against the hashed password in the database! How cool is that?
    
      Extra Badges
      - Finally, the Passport accepts an optional array of "badges", which are extra "stuff" that you want to add... usually to activate other features.
    
      We only need to pass one: a new CsrfTokenBadge(). This is because our login form is protected by a CSRF token. Previously, we checked that manually. Lame!
    
      But no more! Pass the string authenticate to the first argument... which just needs to match the string used when we generate the token in the template: login.html.twig. If I search for csrf_token... there it is!
    
      For the second argument, pass the submitted CSRF token: $request->request->get('_csrf_token'), which you can also see in the login form.
    
      And... done! Just by passing the badge, the CSRF token will be validated.

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -22,7 +22,9 @@ use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
 use Symfony\Component\Security\Guard\PasswordAuthenticatedInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
@@ -39,7 +41,21 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator {
 		$password = $request->request->get('password');
 
 		return new Passport(
-			new UserBadge($email)
+			new UserBadge($email, function($userIdentifier) {
+				$user = $this->entityManager
+					->getRepository(User::class)
+					->findOneBy(['email' => $userIdentifier]);
+
+				if (!$user) {
+					throw new UserNotFoundException();
+				}
+
+				return $user;
+			}),
+			new PasswordCredentials($password),
+			[
+				new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token'))
+			]
 		);
 	}
 
