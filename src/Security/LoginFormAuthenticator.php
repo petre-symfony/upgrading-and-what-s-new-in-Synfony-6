113d3621a34f317c57af600dd69dcc6eb8a52ecb
petrero <petrero@gmail.com>
2022-06-02 08:01:48 +0300

    13.3. Custom Authenticator authenticate() Method
      Oh, and while we don't need to do this, I'm also going to pass a new RememberMeBadge(). If you use the "Remember Me" system, then you need to pass this badge. It tells the system that you opt "into" having a remember me cookie set if the user logs in using this authenticator. But you still need to have a "Remember Me" checkbox here in login form... for it to work.

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -23,6 +23,7 @@ use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticato
 use Symfony\Component\Security\Guard\PasswordAuthenticatedInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
@@ -54,7 +55,8 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator {
 			}),
 			new PasswordCredentials($password),
 			[
-				new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token'))
+				new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
+				(new RememberMeBadge())
 			]
 		);
 	}
